e57411dc6b4f9ca8abaab9232407fc8dc7bca5ef
Sebastian Rodriguez Suarez <129565458+srodriguezsu@users.noreply.github.com>
2025-04-25 15:06:58 -0500

    Tests passed

--- homework/word_count.py
+++ homework/word_count.py
@@ -7,7 +7,7 @@ import glob
 import os.path
 import time
 from itertools import groupby
-
+import string
 
 #
 # Escriba la funcion que  genere n copias de los archivos de texto en la
@@ -17,27 +17,34 @@ from itertools import groupby
 # original se llama text0.txt, el archivo generado se llamará text0_1.txt,
 # text0_2.txt, etc.
 #
+
 def copy_raw_files_to_input_folder(n):
     """Funcion copy_files"""
 
+    if not os.path.exists("files/input"):
+        os.makedirs("files/input")
+    for file in glob.glob("files/raw/*"):
+        for i in range(1, n + 1):
+            with open(file, "r", encoding="utf-8") as f:
+                with open(
+                    f"files/input/{os.path.basename(file).split('.')[0]}_{i}.txt",
+                    "w",
+                    encoding="utf-8",
+                ) as f2:
+                    f2.write(f.read())
 
-#
-# Escriba la función load_input que recive como parámetro un folder y retorna
-# una lista de tuplas donde el primer elemento de cada tupla es el nombre del
-# archivo y el segundo es una línea del archivo. La función convierte a tuplas
-# todas las lineas de cada uno de los archivos. La función es genérica y debe
-# leer todos los archivos de folder entregado como parámetro.
-#
-# Por ejemplo:
-#   [
-#     ('text0'.txt', 'Analytics is the discovery, inter ...'),
-#     ('text0'.txt', 'in data. Especially valuable in ar...').
-#     ...
-#     ('text2.txt'. 'hypotheses.')
-#   ]
-#
+
+
+
 def load_input(input_directory):
     """Funcion load_input"""
+
+    sequence = []
+    files = glob.glob(f"{input_directory}/*")
+    with fileinput.input(files=files) as f:
+        for line in f:
+            sequence.append((fileinput.filename(), line))
+    return sequence
 
 
 #
@@ -45,25 +52,21 @@ def load_input(input_directory):
 # función anterior y retorna una lista de tuplas (clave, valor). Esta función
 # realiza el preprocesamiento de las líneas de texto,
 #
+
+
 def line_preprocessing(sequence):
     """Line Preprocessing"""
+    sequence = [
+        (key, value.translate(str.maketrans("", "", string.punctuation)).lower())
+        for key, value in sequence
+    ]
+
+    return sequence
 
 
-#
-# Escriba una función llamada maper que recibe una lista de tuplas de la
-# función anterior y retorna una lista de tuplas (clave, valor). En este caso,
-# la clave es cada palabra y el valor es 1, puesto que se está realizando un
-# conteo.
-#
-#   [
-#     ('Analytics', 1),
-#     ('is', 1),
-#     ...
-#   ]
-#
 def mapper(sequence):
     """Mapper"""
-
+    return [(word, 1) for _, value in sequence for word in value.split()]
 
 #
 # Escriba la función shuffle_and_sort que recibe la lista de tuplas entregada
@@ -76,8 +79,10 @@ def mapper(sequence):
 #     ...
 #   ]
 #
+
 def shuffle_and_sort(sequence):
     """Shuffle and Sort"""
+    return sorted(sequence, key=lambda x: x[0])
 
 
 #
@@ -88,6 +93,14 @@ def shuffle_and_sort(sequence):
 #
 def reducer(sequence):
     """Reducer"""
+    result = []
+    for key, group in groupby(sequence, lambda x: x[0]):
+        result.append((key, sum(value for _, value in group)))
+    return result
+
+
+
+
 
 
 #
@@ -96,6 +109,12 @@ def reducer(sequence):
 #
 def create_ouptput_directory(output_directory):
     """Create Output Directory"""
+
+    if os.path.exists(output_directory):
+        for file in glob.glob(f"{output_directory}/*"):
+            os.remove(file)
+        os.rmdir(output_directory)
+    os.makedirs(output_directory)
 
 
 #
@@ -108,6 +127,9 @@ def create_ouptput_directory(output_directory):
 #
 def save_output(output_directory, sequence):
     """Save Output"""
+    with open(f"{output_directory}/part-00000", "w", encoding="utf-8") as f:
+        for key, value in sequence:
+            f.write(f"{key}\t{value}\n")
 
 
 #
@@ -116,16 +138,29 @@ def save_output(output_directory, sequence):
 #
 def create_marker(output_directory):
     """Create Marker"""
+    with open(f"{output_directory}/_SUCCESS", "w", encoding="utf-8") as f:
+        f.write("")
 
 
 #
 # Escriba la función job, la cual orquesta las funciones anteriores.
 #
+
 def run_job(input_directory, output_directory):
     """Job"""
 
+    sequence = load_input(input_directory)
+    sequence = line_preprocessing(sequence)
+    sequence = mapper(sequence)
+    sequence = shuffle_and_sort(sequence)
+    sequence = reducer(sequence)
+    create_ouptput_directory(output_directory)
+    save_output(output_directory, sequence)
+    create_marker(output_directory)
 
-if __name__ == "__main__":
+
+
+if __name__ == "_main_":
 
     copy_raw_files_to_input_folder(n=1000)
 
